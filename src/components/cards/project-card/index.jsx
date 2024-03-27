8bf496a0f4b1bf19e028c0909d6d541d24bf351f
vohidovich5557 <shokhuk006@gmail.com>
2024-03-27 19:35:14 +0500

    1

--- src/components/cards/project-card/index.jsx
+++ src/components/cards/project-card/index.jsx
@@ -13,7 +13,7 @@ export const ProjectCard = ({img, aos, title, link, link2, linktext, linktext2, 
          <div className="w-[300px] h-[567px] lg:w-[349px] flex flex-col items-center justify-center shadow-lg shadow-gray-500 rounded-[15px] mb-[30px]" key={id} data-aos={aos} data-aos-duration="3000">
             <img src={img} alt="img" className="w-full h-auto  translate-y-[-52px]" />
             <div className=" pl-[28px] pr-[31px]">
-            <h3 className="text-center text-2xl text-secondary font-medium mb-[17px]">{title}</h3>
+            <h3 className="text-center text-2xl text-secondary font-medium mb-[17px]"><a href="https://giper-mart-kappa.vercel.app/">{title}</a></h3>
             <p className="text-center mb-[12px] text-md text-black/50">{text}</p>
             <div className="flex items-center justify-center gap-1 mb-[20px]">
                 <p className="text-md text-secondary font-normal">Tech stack:</p>
